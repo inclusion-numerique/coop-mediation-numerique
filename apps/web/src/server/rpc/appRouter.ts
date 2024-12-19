87d9ea207afad8d889b580b963da8cc423285ba2
Hugues Maignol <hugues@maignol.com>
2024-12-19 17:59:00 +0100

    feat: assistant chat ui

--- apps/web/src/server/rpc/appRouter.ts
+++ apps/web/src/server/rpc/appRouter.ts
@@ -12,6 +12,7 @@ import { beneficiairesRouter } from '@app/web/server/rpc/beneficiaires/beneficia
 import { mediateursRouter } from '@app/web/server/rpc/mediateur/mediateursRouter'
 import { lieuActiviteRouter } from '@app/web/server/rpc/lieu-activite/lieuActiviteRouter'
 import { apiClientRouter } from '@app/web/server/rpc/apiClient/apiClientRouter'
+import { assistantRouter } from '@app/web/server/rpc/assistant/assistantRouter'
 import { rdvServicePublicRouter } from '@app/web/server/rpc/rdv-service-public/rdvServicePublicRouter'
 
 export const appRouter = router({
@@ -28,6 +29,7 @@ export const appRouter = router({
   conseillersNumerique: conseillersNumeriqueRouter,
   lieuActivite: lieuActiviteRouter,
   apiClient: apiClientRouter,
+  assistant: assistantRouter,
   rdvServicePublic: rdvServicePublicRouter,
 })
 // export type definition of API
