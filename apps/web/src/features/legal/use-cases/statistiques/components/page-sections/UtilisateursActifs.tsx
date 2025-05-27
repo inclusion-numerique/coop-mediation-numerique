f3f940df6a537cc01f783f74160dd94abe21f940
Marc Gavanier <marc.gavanier@protonmail.com>
2025-05-27 11:15:04 +0200

    fix: statistiques utilisateurs actifs
    (cherry picked from commit 2caa084d12be2d9abb46c306c1a7ebd116417d16)

--- apps/web/src/features/legal/use-cases/statistiques/components/page-sections/UtilisateursActifs.tsx
+++ apps/web/src/features/legal/use-cases/statistiques/components/page-sections/UtilisateursActifs.tsx
@@ -9,11 +9,12 @@ export const UtilisateursActifs = ({
   <>
     <h2 className="fr-h3 fr-mb-12v fr-flex fr-align-items-center fr-flex-gap-3v">
       <span className="ri-map-pin-user-fill fr-text--regular" />
-      Impact sur nos utilisateurs : les médiateurs numériques
+      Impact sur nos utilisateurs&nbsp;: les médiateurs numériques
     </h2>
     <ChartCard
       title="Utilisateurs actifs"
       chartTitle="Évolution du nombre d’utilisateurs actifs"
+      legend="Un médiateur et/ou conseiller numérique est considéré actif lorsqu’il a renseigné au moins un compte-rendu d’activité sur les 30 derniers jours. Un coordinateur est considéré actif lorsqu’il c’est connecté au moins 1 fois sur les 30 derniers jours."
       color="brown-caramel"
       icon="ri-account-circle-line"
       {...utilisateursActifs}
