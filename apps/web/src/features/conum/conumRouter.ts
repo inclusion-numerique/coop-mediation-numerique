2849768f18d1c9b7ca31f3a71b0c7e99982080b3
Hugues Maignol <hugues@maignol.com>
2025-05-27 15:38:45 +0200

    qa: rename .ts files with same name as .tsx files (osx compat)
    (cherry picked from commit f8ce65580b5a6343af27fd63308b2ff8d840fdbc)

--- apps/web/src/features/conum/conumRouter.ts
+++ apps/web/src/features/conum/conumRouter.ts
@@ -1,8 +1,8 @@
 import { protectedProcedure, router } from '@app/web/server/rpc/createRouter'
 import { enforceIsAdmin } from '@app/web/server/rpc/enforceIsAdmin'
-import { syncProfiles } from './use-cases/sync-profiles/syncProfiles'
-import { updateInfo } from './use-cases/update-info/updateInfo'
-import { updateStructureReferent } from './use-cases/update-structure-referent/updateStructureReferent'
+import { syncProfiles } from './use-cases/sync-profiles/sync-profiles'
+import { updateInfo } from './use-cases/update-info/update-info'
+import { updateStructureReferent } from './use-cases/update-structure-referent/update-structure-referent'
 
 export const conumRouter = router({
   syncProfiles: protectedProcedure.mutation(
