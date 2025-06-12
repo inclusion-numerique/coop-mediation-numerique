3aaefa88bbdc1460c63b0d77e85393da8d5524b4
Hugues Maignol <hugues@maignol.com>
2025-06-12 15:43:50 +0200

    ui: remove cra creation modal in beneficiaire page

--- apps/web/src/app/coop/(sidemenu-layout)/mes-beneficiaires/[beneficiaireId]/(consultation)/BeneficiaireEnregistrerUneActivite.tsx
+++ apps/web/src/app/coop/(sidemenu-layout)/mes-beneficiaires/[beneficiaireId]/(consultation)/BeneficiaireEnregistrerUneActivite.tsx
@@ -1,18 +1,12 @@
 'use client'
 
-import CreateCraModalContent from '@app/web/features/activites/use-cases/cra/components/CreateCraModal/CreateCraModalContent'
 import type { BeneficiaireCraData } from '@app/web/features/beneficiaires/validation/BeneficiaireValidation'
+import { encodeSerializableState } from '@app/web/utils/encodeSerializableState'
 import Button from '@codegouvfr/react-dsfr/Button'
 import { createModal } from '@codegouvfr/react-dsfr/Modal'
 
-export const CreateCraBeneficiaireModalDefinition = createModal({
-  id: 'create-cra-beneficiaire',
-  isOpenedByDefault: false,
-})
-
 const BeneficiaireEnregistrerUneActivite = ({
   beneficiaire,
-  displayName,
   label = 'Enregistrer une activité',
   size,
 }: {
@@ -20,28 +14,29 @@ const BeneficiaireEnregistrerUneActivite = ({
   displayName: string
   label?: string
   size?: 'small' | 'medium' | 'large'
-}) => (
-  <>
+}) => {
+  const craDefaultValues = {
+    beneficiaire,
+  }
+  const retour = `/coop/mes-beneficiaires/${beneficiaire.id}/accompagnements`
+
+  const createCraPath = `/coop/mes-activites/cra/individuel?v=${encodeSerializableState(craDefaultValues)}${
+    retour ? `&retour=${retour}` : ''
+  }`
+
+  return (
     <div className="fr-flex fr-direction-column fr-flex-gap-2v">
       <Button
         iconId="fr-icon-add-line"
         size={size}
-        onClick={CreateCraBeneficiaireModalDefinition.open}
+        linkProps={{
+          href: createCraPath,
+        }}
       >
         {label}
       </Button>
     </div>
-    <CreateCraBeneficiaireModalDefinition.Component
-      title={`Ajouter une activité pour ${displayName}`}
-    >
-      <CreateCraModalContent
-        retour={`/coop/mes-beneficiaires/${beneficiaire.id}/accompagnements`}
-        craDefaultValues={{ beneficiaire }}
-        atelier={false}
-        onClose={CreateCraBeneficiaireModalDefinition.close}
-      />
-    </CreateCraBeneficiaireModalDefinition.Component>
-  </>
-)
+  )
+}
 
 export default BeneficiaireEnregistrerUneActivite
