94d68685154cb4e7a269d77b210258d1985f1f57
Hugues Maignol <hugues@maignol.com>
2025-09-09 15:43:41 +0200

    chore: update db storage type

--- packages/cdk/src/ProjectStack.ts
+++ packages/cdk/src/ProjectStack.ts
@@ -154,8 +154,9 @@ export class ProjectStack extends TerraformStack {
       backupSameRegion: false,
       backupScheduleFrequency: 24,
       backupScheduleRetention: 14,
-      volumeType: 'bssd', // Block storage
-      volumeSizeInGb: 15,
+      volumeType: 'sbs_15k',
+      volumeSizeInGb: 30,
+      encryptionAtRest: true,
       settings: {
         // Custom max connections
         max_connections: '500',
