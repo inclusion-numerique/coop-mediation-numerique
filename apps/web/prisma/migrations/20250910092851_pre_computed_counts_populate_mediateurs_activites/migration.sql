448173e0c124d16da51bd369effcd5791f34bde9
Hugues Maignol <hugues@maignol.com>
2025-09-10 15:19:24 +0200

    fix: migration conflicts

--- apps/web/prisma/migrations/20250910092851_pre_computed_counts_populate_mediateurs_activites/migration.sql
+++ apps/web/prisma/migrations/20250910092851_pre_computed_counts_populate_mediateurs_activites/migration.sql
@@ -1,4 +1,4 @@
-CREATE TEMP TABLE tmp_mediateur_counts AS
+CREATE TEMP TABLE tmp_mediateur_counts_activites AS
 SELECT activites.mediateur_id, 
        COUNT(*)::int AS activites_count, 
        SUM(activites.accompagnements_count) AS accompagnements_count
@@ -6,11 +6,11 @@ FROM activites
 WHERE activites.suppression IS NULL
 GROUP BY activites.mediateur_id;
 
-ALTER TABLE tmp_mediateur_counts ADD PRIMARY KEY (mediateur_id);
-ANALYZE tmp_mediateur_counts;
+ALTER TABLE tmp_mediateur_counts_activites ADD PRIMARY KEY (mediateur_id);
+ANALYZE tmp_mediateur_counts_activites;
 
 UPDATE mediateurs
 SET activites_count = t.activites_count,
     accompagnements_count = t.accompagnements_count
-FROM tmp_mediateur_counts t
+FROM tmp_mediateur_counts_activites t
 WHERE mediateurs.id = t.mediateur_id;
