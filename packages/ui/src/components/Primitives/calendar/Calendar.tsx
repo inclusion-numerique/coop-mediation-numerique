746f261885da7d1017313e6bae156b8119354ba1
Hugues Maignol <hugues@maignol.com>
2025-11-14 14:48:12 +0100

    qa: lint fix

--- packages/ui/src/components/Primitives/calendar/Calendar.tsx
+++ packages/ui/src/components/Primitives/calendar/Calendar.tsx
@@ -1,8 +1,8 @@
 import { dateAsIsoDay } from '@app/web/utils/dateAsIsoDay'
 import { dateFormatter } from '@app/web/utils/formatDate'
 import classNames from 'classnames'
-import ReactCalendar from 'react-calendar'
 import type { CalendarProps as ReactCalendarProps } from 'react-calendar'
+import ReactCalendar from 'react-calendar'
 import styles from './Calendar.module.css'
 
 const today = dateAsIsoDay(new Date())
