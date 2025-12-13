1afe20da8b09287f40c798ef599f1ca9c707ce25
Hugues Maignol <hugues@maignol.com>
2025-12-13 15:43:11 +0700

    chore: disable dataspace integration on mocked envs

--- apps/web/src/external-apis/dataspace/dataspaceApiClient.integration.ts
+++ apps/web/src/external-apis/dataspace/dataspaceApiClient.integration.ts
@@ -3,8 +3,17 @@ import {
   isDataspaceApiError,
   isDataspaceApiNotFound,
 } from '@app/web/external-apis/dataspace/dataspaceApiClient'
+import { ServerWebAppConfig } from '@app/web/ServerWebAppConfig'
 
 describe('dataspaceApiClient', () => {
+  beforeAll(() => {
+    if (ServerWebAppConfig.Dataspace.isMocked) {
+      throw new Error(
+        'Dataspace API is mocked, but should not be mocked in integration tests',
+      )
+    }
+  })
+
   describe('getMediateurFromDataspaceApi', () => {
     it.only('should return null for a test email that does not exist', async () => {
       const result = await getMediateurFromDataspaceApi({
